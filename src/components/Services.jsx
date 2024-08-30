4be06a07088a27eb7303a6b60dd8d9f135c69787
Ayush Rai <raiayushmaan1092@gmail.com>
2024-08-30 09:52:18 +0530

    services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import service from '../assets/SEVICESSS.png'
 
 function Services() {
   return (
     <div className=' w-full sm:h-[700px] mt-10'>
       <h1 className=' text-xl sm:text-3xl font-extrabold font-sans text-center sm:tracking-widest mb-2 z-10 relative'>OUR SERVICES</h1>
       <div className=' w-[110px] sm:w-[190px] h-1 bg-blue-600 m-auto mb-4 z-10 relative'></div>
-      <div className=' sm:mt-[-60px] mt-10 flex justify-center'>
-        <img src="./ServiceInc.png" width={'2000px'} alt="" />
+      <div className=' sm:mt-[-60px] mt-10 sm:px-8 flex justify-center'>
+        <img src={service} width={'2000px'} alt="" />
       </div>
     </div>
   )

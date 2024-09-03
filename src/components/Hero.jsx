288b595cc53e9718faec1c2ab26b8cfb02826e8a
Ayush Rai <raiayushmaan1092@gmail.com>
2024-09-03 11:11:10 +0530

    mistake

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,10 +9,10 @@ function Hero() {
       <div className=' absolute w-full  sm:h-[840px] top-[120px] bg-red-50'>
         <video src={video} autoPlay loop muted className=' inset-0 w-full h-full object-cover'></video>
       </div>
-      <div className=" text-white  flex flex-col absolute top-[120px] w-[100%] sm:h-[840px] h-[220px] justify-center items-center bg-black opacity-55 ">
+      <div className=" text-white  flex flex-col absolute top-[120px] w-[100%] sm:h-[840px] h-[57vw] justify-center items-center bg-black opacity-55 ">
         <h1 className="text-center sm:text-8xl text-2xl tracking-wider font-extrabold font-sans sm:mt-[-280px] md:mt-[-350px] lg:mt-[-400px] xl:mt-[-250px]">Invertis Innovation &</h1>
         <h1 className="text-center sm:text-7xl sm:mx-4 text-2xl tracking-widest font-extrabold font-sans sm:mt-[-10px] md:mt-[-5px] lg:mt-[-2px] xl:mt-[15px]">Incubation Foundation</h1>
-        <h3 className="text-center sm:text-2xl mt-5 text-yellow-300 font-semibold tracking-tight">This is the home for Inverti's thriving community of innovators & entrepreneurs.</h3>
+        <h3 className="text-center sm:text-2xl mt-5 text-yellow-300 font-semibold tracking-tight">This is the home for Invertis thriving community of innovators & entrepreneurs.</h3>
       </div>
     </div>
   )
